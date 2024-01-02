17bf6a2babffcf9d363a94f8fde4d56e98085435
Avas <avas.bajracharya@renegadeinsurance.com>
2024-01-02 14:00:45 +0545

    feat: added products controllers

---
+++ controllers/products.controller.js
@@ -0,0 +1,12 @@
+const getAllProducts = async (req, res) => {
+  res.send("Hello World");
+};
+
+const getProductById = async (req, res) => {
+  res.send("Hello World");
+};
+
+module.exports = {
+  getAllProducts,
+  getProductById,
+};
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const {
+  getAllProducts,
+  getProductById,
+} = require("../controllers/products.controller");
 
 const router = express.Router();
 
 router.get("/", getAllProducts);
-router.get("/:id", getSingleProduct);
+router.get("/:id", getProductById);
 
 module.exports = router;
