f5f7f88b8427e0bbebf7dd22c3edef8bb058d47d
Avas <avas.bajracharya@renegadeinsurance.com>
2024-01-03 17:40:06 +0545

    feat: added post route for adding products

--- mongodb/models/products.js
+++ mongodb/models/products.js
@@ -1,6 +1,6 @@
 const { default: mongoose } = require("mongoose");
 const productsSchema = require("../schema/products");
 
-const productsModel = new mongoose.model("Products", productsSchema);
+const ProductsModel = new mongoose.model("Products", productsSchema);
 
-module.exports = productsModel;
+module.exports = ProductsModel;
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const {
   getAllProducts,
   getProductById,
+  addProducts,
 } = require("../controllers/products.controller");
 
 const router = express.Router();
 
-router.post("/", po)
+router.post("/", addProducts);
 router.get("/", getAllProducts);
 router.get("/:id", getProductById);
 
