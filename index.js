4fdcf3dc8167f859ed34e698b12d418abbc8bf3a
Avas <avas.bajracharya@renegadeinsurance.com>
2024-01-03 13:49:12 +0545

    feat: enhanced server code

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
 const productsRouter = require("./routes/products.routes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+dotenv.config();
+app.use(cors());
 app.use(express.json());
 app.use("/api/v1/products", productsRouter);
 
-app.listen(PORT, () => console.log(`Servet listening at PORT ${PORT}`));
+const startServer = async () => {
+  try {
+    app.listen(PORT, () => console.log(`Servet listening at PORT ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
